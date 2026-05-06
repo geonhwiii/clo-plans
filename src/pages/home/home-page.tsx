201d7ebc83320457b91a3b7d26fbb677592a4ce0
geonhwiii <geonhwiii@users.noreply.github.com>
2026-05-06 13:57:03 +0900

    feat: add tailwind & path settings

---
+++ src/pages/home/home-page.tsx
@@ -0,0 +1,9 @@
+export function HomePage() {
+  return (
+    <div>
+      <div>Header</div>
+      <div>Main</div>
+      <div>Footer</div>
+    </div>
+  );
+}
---
+++ src/shared/ui/button.tsx
@@ -0,0 +1,3 @@
+export function Button() {
+  return <button className="rounded-md bg-blue-500 p-2 text-white">Click me</button>;
+}
